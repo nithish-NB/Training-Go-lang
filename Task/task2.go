9f1f524947972bc939a8b13dc0a41b8eab1c495b
nithis <nithish1star@gmail.com>
2023-08-11 09:23:44 +0530

    commited again

--- Task/task2.go
+++ Task/task2.go
@@ -1,1 +1,11 @@
 package main
+
+import "fmt"
+
+func main() {
+
+	println("after")
+	fmt.Println("hello world")      //after exceution goes to new line
+	fmt.Print("hello world ")       //fter exceution doesn't goes to new line
+	fmt.Printf("%s", "hello world") // we need to use access specifiers
+}
