6befb316e60bacd0b4fb7376641d69375c080ee8
nithis <nithish1star@gmail.com>
2023-08-11 09:47:08 +0530

    Task-3 cosntant

---
+++ Task/task3.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const a = 12
+	const b = 6
+
+	// addition
+	const c = a + b
+	fmt.Println(c)
+
+	// subtraction
+	const d = a - b
+	fmt.Println(d)
+
+	// multiplication
+	const e = a * b
+	fmt.Println(e)
+
+	//divison
+	const f = a / b
+	fmt.Println(f)
+
+	//modulus
+	const g = a % b
+	fmt.Println(g)a
+
+}
